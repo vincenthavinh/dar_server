a5172ea3241912644d632e4c42b4d0e4bd78d3c0
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-10-09 01:13:40 +0200

    [Add] Servlet Test, web.xml de base et overview.html

---
+++ src/servlets/Test.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+	    resp.setCharacterEncoding( "UTF-8" );
+	    
+	    PrintWriter out = resp.getWriter();
+	    
+	    Enumeration<String> parameterNames = req.getParameterNames();
+	    while (parameterNames.hasMoreElements()) {
+	        String param = (String) parameterNames.nextElement();
+	        out.println(param + " = [" + req.getParameter(param) + "]<br/>");
+	    }
+	}
+}
